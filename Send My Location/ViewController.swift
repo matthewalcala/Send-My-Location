54ba03511420ec7e6856f35ef085e3bb3023c0c7
Matthew Alcala <matthewalcala@mmbp.local>
2015-11-29 00:17:51 -0800

    ver 2

--- Send My Location/ViewController.swift
+++ Send My Location/ViewController.swift
@@ -38,6 +38,12 @@ class ViewController: UIViewController, MFMessageComposeViewControllerDelegate, 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+        // hi
+    }
+    
+    func hi(){
+        
     }
 
     @IBAction func takePhoto(sender: AnyObject) {
