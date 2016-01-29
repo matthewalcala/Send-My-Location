7866be7609cf5362c5848c8cc8af25c8dd9adce2
ninjcoda <matt@mattalcala.com>
2016-01-29 10:09:50 -0800

    Stable version with alert message added

---
+++ Send My Location/TwilioHelper.swift
@@ -0,0 +1,12 @@
+//
+//  TwilioHelper.swift
+//  Send My Location
+//
+//  Created by Matthew Alcala on 12/14/15.
+//  Copyright © 2015 ninjabandito. All rights reserved.
+//
+
+import Foundation
+
+
+
